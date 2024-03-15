ee36c9475dc96bcd0546fc922a64a43aae726c5f
Ayush Srivastav <ayushsrivastav11@gmail.com>
2024-03-15 21:55:19 +0530

    correct solution

--- 2095.js
+++ 2095.js
@@ -13,14 +13,20 @@ var deleteMiddle = function (head) {
   let prev = null;
   let current1 = head,
     current2 = head;
+  let len = 0;
   while (current2 !== null) {
-    prev = current1;
-    current1 = current1.next;
+    len += 1;
     current2 = current2.next;
     if (current2 !== null) {
+      len += 1;
+      prev = current1;
+      current1 = current1.next;
       current2 = current2.next;
     } else current2 = null;
   }
-  prev.next = current1.next;
-  return head;
+  if (len == 1) return null;
+  else {
+    if (current1 != null && prev?.next != null) prev.next = current1.next;
+    return head;
+  }
 };

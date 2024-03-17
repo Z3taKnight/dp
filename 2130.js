09fc2b30a00781ad7b1ac8fd3e78abe8b3d98006
Ayush Srivastav <ayushsrivastav11@gmail.com>
2024-03-17 13:06:29 +0530

    2130 solution

---
+++ 2130.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function (head) {
+  let slow = head;
+  let fast = head;
+  let maxSum = Number.MIN_VALUE;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  let prev = null;
+  let current = slow;
+  while (current !== null) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  let tail = prev;
+  while (head !== tail && head && tail) {
+    maxSum = Math.max(maxSum, head?.val + tail?.val);
+    head = head.next;
+    tail = tail.next;
+  }
+
+  prev = null;
+  while (current !== null) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return maxSum;
+};

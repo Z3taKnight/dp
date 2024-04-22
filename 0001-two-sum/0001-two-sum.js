694f92917b3ba9e67ada260d3ca0f54ebc0419e8
Ayush Srivastav <52807832+ayushhsrivastav@users.noreply.github.com>
2024-04-22 21:28:16 +0530

    Time: 66 ms (54.3%), Space: 50.8 MB (46.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let hashTable = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (hashTable.has(target - nums[i])) {
+            return [hashTable.get(target - nums[i]), i];
+        } else {
+            hashTable.set(nums[i], i);
+        }
+    }
+};

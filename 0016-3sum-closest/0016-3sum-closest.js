ed5a4381494242b232bd4890dfcfb7e1f529f174
Ayush Srivastav <52807832+ayushhsrivastav@users.noreply.github.com>
2024-08-17 14:07:40 +0530

    Time: 60 ms (94.07%), Space: 50.8 MB (57.46%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+    nums.sort((a, b) => a - b);
+    let closestSum = nums[0] + nums[1] + nums[2];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            let currentSum = nums[i] + nums[left] + nums[right];
+
+            if (currentSum === target) {
+                return currentSum;
+            }
+
+            if (Math.abs(target - currentSum) < Math.abs(target - closestSum)) {
+                closestSum = currentSum;
+            }
+
+            if (currentSum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
+};

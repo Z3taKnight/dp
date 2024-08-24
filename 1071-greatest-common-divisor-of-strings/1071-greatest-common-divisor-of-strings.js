32ba94df887cf1f8d84a1ba95c574df0765dbc5b
Ayush Srivastav <52807832+ayushhsrivastav@users.noreply.github.com>
2024-08-24 15:36:35 +0530

    Time: 51 ms (69.15%), Space: 49.1 MB (63.64%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+    if (str1 + str2 !== str2 + str1) return ''
+
+    let gcd = function (x, y) {
+        if (!y) return x
+        return gcd(y, x % y)
+    }
+
+    let gcdVal = gcd(str1.length, str2.length)
+    return str1.slice(0, gcdVal)
+};

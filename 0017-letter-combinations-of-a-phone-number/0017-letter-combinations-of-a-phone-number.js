d418ecb52a25b88171201dc82f8f74c46c66c235
Ayush Srivastav <52807832+ayushhsrivastav@users.noreply.github.com>
2024-08-24 15:06:27 +0530

    Time: 47 ms (78.85%), Space: 48.4 MB (91.17%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+    if (!digits) return [];
+    let obj = {
+        2: ["a", "b", "c"],
+        3: ["d", "e", "f"],
+        4: ["g", "h", "i"],
+        5: ["j", "k", "l"],
+        6: ["m", "n", "o"],
+        7: ["p", "q", "r", "s"],
+        8: ["t", "u", "v"],
+        9: ["w", "x", "y", "z"],
+    };
+
+    let arr = [];
+    let digitsArr = digits.split("");
+    while (digitsArr.length > 0) {
+        if (arr.length == 0) arr.push(...obj[digitsArr[0]]);
+        else {
+            let temp = [];
+            for (let i = 0; i < arr.length; i++) {
+                for (let j = 0; j < obj[digitsArr[0]].length; j++) {
+                    temp.push(arr[i] + obj[digitsArr[0]][j]);
+                }
+            }
+            arr = temp;
+        }
+        digitsArr.shift();
+    }
+    return arr;
+};

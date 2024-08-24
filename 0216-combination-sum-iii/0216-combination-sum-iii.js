97b93bda04ed729327bdfb6a0ddcdff6eed0a8f6
Ayush Srivastav <52807832+ayushhsrivastav@users.noreply.github.com>
2024-08-24 15:18:25 +0530

    Time: 46 ms (89.97%), Space: 49.5 MB (34.51%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function (k, n) {
+    let arr = [];
+    let temp = [];
+
+    function tracking(num, sum) {
+        if (sum === 0 && temp.length === k) {
+            arr.push([...temp]);
+            return;
+        }
+
+        if (num > 9 || num > sum || temp.length > k) {
+            return;
+        }
+
+        temp.push(num);
+
+        tracking(num + 1, sum - num);
+        temp.pop();
+        tracking(num + 1, sum);
+    }
+
+    tracking(1, n);
+    return arr;
+};
